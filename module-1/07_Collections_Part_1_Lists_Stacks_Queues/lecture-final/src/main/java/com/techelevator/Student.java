420a84a4045669446cd2a2daa489e03f111cbc8a
Tom Beerbower <thomas@techelevator.com>
2022-05-24 12:05:22 -0400

    push 07_Collections_Part_1_Lists_Stacks_Queues lecture-final & 08_Collections_Part_2_Maps tutorial

---
+++ module-1/07_Collections_Part_1_Lists_Stacks_Queues/lecture-final/src/main/java/com/techelevator/Student.java
@@ -0,0 +1,13 @@
+package com.techelevator;
+
+public class Student {
+
+    public String getAddress() {
+        return "123 MAIN ST";
+    }
+
+    public String getName() {
+        return "John Doe";
+    }
+
+}
