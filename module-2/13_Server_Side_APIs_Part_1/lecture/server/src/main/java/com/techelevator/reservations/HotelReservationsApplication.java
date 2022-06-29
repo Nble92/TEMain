d55250d6db0db8aca2fdc3fc20820544fe8cdd01
John Noble <cartoo221@gmail.com>
2022-06-29 12:21:26 -0400

    lock

--- module-2/13_Server_Side_APIs_Part_1/lecture/server/src/main/java/com/techelevator/reservations/HotelReservationsApplication.java
+++ module-2/13_Server_Side_APIs_Part_1/lecture/server/src/main/java/com/techelevator/reservations/HotelReservationsApplication.java
@@ -3,6 +3,9 @@ package com.techelevator.reservations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//this turns the application into an api
+//waiting to take requests
+
 @SpringBootApplication
 public class HotelReservationsApplication {
 
--- module-2/13_Server_Side_APIs_Part_1/lecture/server/src/main/java/com/techelevator/reservations/controllers/HotelController.java
+++ module-2/13_Server_Side_APIs_Part_1/lecture/server/src/main/java/com/techelevator/reservations/controllers/HotelController.java
@@ -5,10 +5,13 @@ import com.techelevator.reservations.dao.MemoryHotelDao;
 import com.techelevator.reservations.dao.MemoryReservationDao;
 import com.techelevator.reservations.dao.ReservationDao;
 import com.techelevator.reservations.model.Hotel;
+import com.techelevator.reservations.model.Reservation;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class HotelController {
 
     private HotelDao hotelDao;
@@ -29,15 +32,59 @@ public class HotelController {
         return hotelDao.list();
     }
 
+
     /**
      * Return hotel information
      *
      * @param id the id of the hotel
      * @return all info for a given hotel
      */
+
+
     @RequestMapping(path = "/hotels/{id}", method = RequestMethod.GET)
     public Hotel get(@PathVariable int id) {
         return hotelDao.get(id);
     }
 
+    //Reservations returns all reservations
+    @RequestMapping(path = "/reservations", method = RequestMethod.GET)
+    public List<Reservation> listReservations() {
+        return reservationDao.findAll();
+    }
+
+
+    //ReservationsByID
+    @RequestMapping(path = "/reservations/{id}", method = RequestMethod.GET)
+    public Reservation reservation(@PathVariable int id) {
+
+        return reservationDao.get(id);
+    }
+
+    //Reservations by hotel
+    @RequestMapping(path = "/hotels/{id}/reservations", method = RequestMethod.GET)
+    public List<Reservation> listReservationsByHotel(@PathVariable int id) {
+        return reservationDao.findByHotel(id);
+    }
+
+    //Add New Reservation
+    @RequestMapping(path = "/reservations", method = RequestMethod.POST)
+    public Reservation addReservation(@RequestBody Reservation reservation) {
+        return reservationDao.create(reservation, reservation.getHotelID());
+    }
+
+    //Filter hotels by query string
+    @RequestMapping(path = "/hotels/filer", method = RequestMethod.GET)
+    public List<Hotel> filterByStateAndCity(@RequestParam String state, @RequestParam(required = false) String city) {
+        List<Hotel> filteredHotel = new ArrayList<>();
+        List<Hotel> hotels = list();
+       /* //return Hotels That Match State
+        for (Hotel hotel : hotels){
+
+
+
+            }*/
+        return null;
+    }
 }
+
+
